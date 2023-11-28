cf0185c1e1a8e0dc878d9b9486b7b1be6e69be4f
Sheila Aliaga <aliaga_sheila@hotmail.com>
2023-11-28 18:45:15 +0100

    test in header

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,17 +1,17 @@
-// import { render, screen } from "@testing-library/react";
-// import Header from ".";
+ import { render, screen } from "@testing-library/react";
+ import Header from ".";
 
-// describe("The page title renders and text is correct", () => {
-//   test("The page title renders on page load", () => {
-//     render(<Header />);
+describe("The page title renders and text is correct", () => {
+   test("The page title renders on page load", () => {
+     render(<Header />);
 
-//     const pageTitle = screen.getByRole("heading", { level: 1 });
-//     expect(pageTitle).toBeInTheDocument();
-//   });
-//   test("The page title follows correct format", () => {
-//     render(<Header />);
+     const pageTitle = screen.getByRole("heading", { level: 1 });
+     expect(pageTitle).toBeInTheDocument();
+   });
+  test("The page title follows correct format", () => {
+     render(<Header />);
 
-//     const pageTitle = screen.getByRole("heading", { level: 1 });
-//     expect(pageTitle).toHaveTextContent(/color guessing game/i);
-//   });
-// });
+     const pageTitle = screen.getByRole("heading", { level: 1 });
+     expect(pageTitle).toHaveTextContent(/color guessing game/i);
+   });
+ });
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h1>Color Guessing Game</h1>
+        </div>
+    )
+}
+
+export default Header
