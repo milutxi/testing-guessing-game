28ae5a8069052dfa20c28171119abf0078f0f562
Sheila Aliaga <aliaga_sheila@hotmail.com>
2023-11-29 15:25:31 +0100

    component color working, TEST FAIL

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import Header from './components/Header';
 import ColorOptions from './components/ColorOptions';
+import Color from './components/Color';
 
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
      <Header />
      <ColorOptions />
+     <Color />
     </div>
   );
 }
--- src/components/Color/index.jsx
+++ src/components/Color/index.jsx
@@ -1,7 +1,57 @@
-const Color = () => {
+import React from "react";
+
+const colors = [
+    {
+    name: "hot pink",
+    code: "FF69B4"
+    },
+    {
+    name: "firebrick",
+    code: "B22222"
+    },
+    {
+    name: "light coral",
+    code: "F08080"
+    },
+    {
+    name: "orange",
+    code: "FFA500"
+    },
+    {
+    name: "pale golden rod",
+    code: "EEE8AA"
+    },
+    {
+    name: "dark olive green",
+    code: "556B2F"
+    },
+    {
+    name: "aqua marine",
+    code: "7FFFD4"
+    },
+    {
+    name: "medium orchid",
+    code: "BA55D3"
+    },
+    {
+    name: "royal blue",
+    code: "4169E1"
+    },
+];
+
+const Color = ({ onClick }) => {
+    
     return (
         <div>
-            
+            {colors.map((color, index) => (
+                    <button 
+                        key={index} 
+                        style={{backgroundColor: `#${color.code}`}}
+                        onClick={() => onClick(color)}>
+                        {color.code}
+                    </button>
+                   
+                    ))}
         </div>
     )
 }
